efac24bd7d649514de3ef5ade825f09c018652a9
lauraM84 <laura.mazza84@gmail.com>
2025-02-20 21:17:52 +0100

    homework

--- esercizi3.js
+++ esercizi3.js
@@ -38,28 +38,116 @@ console.log(fromCToF(array1))
 
 //3) fare la media dei valori
 
-function average(arrayOfNumbers) {
+function findAverage(arrayOfNumbers) {
 
-    const newArray = [];
+    let sum = 0
+
+    for (let i = 0; i < arrayOfNumbers.length; i++) {
+        const element = arrayOfNumbers[i];
+        sum = sum + element
+
+    }
+
+    const average = sum / arrayOfNumbers.length
+    return average
+}
+const average = findAverage(array1)
+console.log(average)
+
+
+
+
+//4) restituire il valore più basso e il più alto
+
+/* function findMax(arrayOfNumbers) {
+    let max = null;
+    for (let i = 0; i < arrayOfNumbers.length; i++) {
+        const element = arrayOfNumbers[i];
+
+        if (max === null || element > max) {
+            max = element
+        }
+    }
+    return max
+}
+const max = findMax(array1)
+console.log(max)
+
+function findMin(arrayOfNumbers) {
+    let min = null;
 
     for (let i = 0; i < arrayOfNumbers.length; i++) {
         const element = arrayOfNumbers[i];
 
-        const average = 
-        
+        if (min === null || element < min) {
+            min = element
+        }
     }
+    return min
+}
+const min = findMin(array1)
+console.log(min) */
+
+function findMinMax(arrayOfNumbers) {
+    let min = null;
+    let max = null
+
+    for (let i = 0; i < arrayOfNumbers.length; i++) {
+        const element = arrayOfNumbers[i];
+
+        if (min === null || element < min) {
+            min = element
+        }
+        if (max === null || element > max) {
+            max = element
+        }
+    }
+    return [min, max]
 
 }
+const minMax = findMinMax(array1)
+console.log(minMax)
 
-//4) restituire il valore più basso e il più alto
+
 
 
 const array2 = ['pippo', 'pluto', 'paperino', 'qui', 'quo', 'qua', 'clarabella', 'minnie']
 
 //5) filtrare tutti i nomi più grandi di 4 caratteri
+
+function findNamesGreaterThan4(arrayOfNames) {
+    let greaters = [];
+    for (let i = 0; i < arrayOfNames.length; i++) {
+        const element = arrayOfNames[i];
+        if (element.length > 4) {
+            greaters.push(element);
+        }
+    }
+    return greaters;
+}
+const namesGreaterThan4 = findNamesGreaterThan4(array2)
+console.log(namesGreaterThan4)
+
+
 //6) trasforamre tutte le stringhe in maiscolo
 //7) mettere Solo le Iniziali in maiuscolo
 //8) restituire una stringa composta dalle iniziali di tutti i nomi
+
+function getInitialOfNames(arrayOfNames) {
+    let initials = "";
+    for (let i = 0; i < arrayOfNames.length; i++) {
+        const element = arrayOfNames[i];
+        const currentInitial = element[0];
+        //initials = initials + currentInitial
+        initials += currentInitial;
+    }
+    return initials;
+}
+const initials = getInitialOfNames(array2)
+console.log(initials)
+
+
+
 //9) contare tutti i caratteri di tutte le stringhe
 //10) eliminare le vocali dalle stringhe
 //11) restituire un array di numeri che sono le lunghezze delle singole parole
